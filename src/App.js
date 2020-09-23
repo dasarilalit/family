4050220d1e123ef79f3d8f6d83adb61c2e4c6fcb
dasarilalit <dasarilalit@gmail.com>
2020-09-23 17:17:22 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <header className="app-header">
+      <span>SONA BIRTHDAY</span>
+     </header>
+  
+
+
+     <img src ="\IMAGES\hi (2).jpg" alt="IMAGES" className="ALL"/>
+        
+     <img src ="\IMAGES\hi (16).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (4).jpg" alt="IMAGES" className="ALL"/>
+      
+     <img src ="\IMAGES\hi (58).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (86).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (13).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (14).jpg" alt="IMAGES" className="ALL"/>
+     
+     <img src ="\IMAGES\hi (17).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (24).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (25).jpg" alt="IMAGES" className="ALL"/>
+
+     <img src ="\IMAGES\hi (38).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (39).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (40).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (51).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (52).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (84).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (53).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (56).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (57).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (67).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (21).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (43).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (81).jpg" alt="IMAGES" className="ALL"/>
+
+     <img src ="\IMAGES\hi (3).jpg" alt="IMAGES" className="ALL"/>
+   
+     <img src ="\IMAGES\hi (5).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (6).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (7).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (8).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (9).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (10).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (11).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (12).jpg" alt="IMAGES" className="ALL"/>
+  
+
+ 
+     <img src ="\IMAGES\hi (15).jpg" alt="IMAGES" className="ALL"/>
+
+     <img src ="\IMAGES\hi (18).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (19).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (20).jpg" alt="IMAGES" className="ALL"/>
+
+     <img src ="\IMAGES\hi (22).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (23).jpg" alt="IMAGES" className="ALL"/>
+    
+     <img src ="\IMAGES\hi (26).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (27).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (28).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (29).jpg" alt="IMAGES" className="ALL"/>
+
+
+     <img src ="\IMAGES\hi (30).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (31).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (32).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (33).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (34).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (35).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (36).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (37).jpg" alt="IMAGES" className="ALL"/>
+     
+     <img src ="\IMAGES\hi (41).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (42).jpg" alt="IMAGES" className="ALL"/>
+    
+
+     <img src ="\IMAGES\hi (44).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (45).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (46).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (47).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (48).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (49).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (50).jpg" alt="IMAGES" className="ALL"/>
+     
+     <img src ="\IMAGES\hi (54).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (55).jpg" alt="IMAGES" className="ALL"/>
+ 
+     <img src ="\IMAGES\hi (59).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (60).jpg" alt="IMAGES" className="ALL"/>
+   
+     <img src ="\IMAGES\hi (62).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (63).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (65).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (66).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (64).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (68).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (69).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (70).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (71).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (72).jpg" alt="IMAGES" className="ALL"/>
+
+     <img src ="\IMAGES\hi (73).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (74).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (75).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (76).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (77).jpg" alt="IMAGES" className="ALL"/>
+
+     
+     <img src ="\IMAGES\hi (82).jpg" alt="IMAGES" className="ALL"/>
+     <img src ="\IMAGES\hi (83).jpg" alt="IMAGES" className="ALL"/>
+ 
+     <img src ="\IMAGES\hi (85).jpg" alt="IMAGES" className="ALL"/>
+    
+ 
+
+
+
+
+   
+       
+            </div>
+
+
+
+
+  );
+}
+
+export default App;
